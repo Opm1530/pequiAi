db01881a118b18457f23df2dc19852d22bfc71dc
Opm1530 <66329029+Opm1530@users.noreply.github.com>
2024-12-31 12:04:30 -0300

    version

--- pages/home/instancias/index.php
+++ pages/home/instancias/index.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Home</title>
+    <title>Instâncias</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -26,9 +26,9 @@ if (!isset($_SESSION['user_id'])) {
         </a>
 
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="./instancias/index.php" class="nav-link">Instâcias</a></li>
-            <li class="nav-item"><a href="../../services/auth/logout.php"class="nav-link">Sair</a></li>
+            <li class="nav-item"><a href="../index.php" class="nav-link " aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link active">Instâncias</a></li>
+            <li class="nav-item"><a href="../../../services/auth/logout.php"class="nav-link">Sair</a></li>
         </ul>
         </header>
     </div>
