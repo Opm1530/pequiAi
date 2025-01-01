b0f98d2ef04de57dc636523bd5c03ca2128c879f
Opm1530 <66329029+Opm1530@users.noreply.github.com>
2025-01-01 15:06:01 -0300

    s

--- pages/home/privateMessage/index.php
+++ pages/home/privateMessage/index.php
@@ -10,14 +10,15 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Home</title>
+    <title>Envio no Privado</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    
 </head>
 <body>
+    
     <div class="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
@@ -26,14 +27,30 @@ if (!isset($_SESSION['user_id'])) {
         </a>
 
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="./instancias/index.php" class="nav-link">Instâcias</a></li>
-            <li class="nav-item"><a href="./privateMessage/index.php" class="nav-link">Instâcias</a></li>
-            <li class="nav-item"><a href="../../services/auth/logout.php"class="nav-link">Sair</a></li>
+            <li class="nav-item"><a href="../index.php" class="nav-link " aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link active">Instâncias</a></li>
+            <li class="nav-item"><a href="../privateMessage/index.php" class="nav-link active">Envio no privado</a></li>
+            <li class="nav-item"><a href="../../../services/auth/logout.php"class="nav-link">Sair</a></li>
         </ul>
         </header>
     </div>
-    <h1>Home</h1>
-
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                <h1>Envio no privado</h1>
+            </div>
+            <div class="col">
+            
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-primary" id="openPopupBtn">Novo Envio</button>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        
+        </table>
+    </div>
+    <script src='./js/script.js'></script>
 </body>
 </html>
