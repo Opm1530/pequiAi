422a6748f3e766b662cab48bbad52e0ec3bfbd21
Opm1530 <66329029+Opm1530@users.noreply.github.com>
2025-01-04 18:53:40 -0300

    Update script.js

--- pages/home/agenteAi/js/script.js
+++ pages/home/agenteAi/js/script.js
@@ -66,7 +66,7 @@ async function checkAPI() {
     
     try {
         const response = await fetch('../../../../services/agent/apiKeyCheck.php');
-        console.log(response)
+        console.log(response.status)
         // Verifica se a resposta foi bem-sucedida
         if (response.data.status != "success") {
             divInputApi.style.display = "flex";
